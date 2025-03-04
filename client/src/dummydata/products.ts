61b1df78021609d05033778001aa0eb567a7ef4a
zerorivals <z_haskell98@outlook.com>
2025-03-03 19:41:12 -0700

    added base files for landing page product page nav bar and some logic for cart and product

---
+++ client/src/dummydata/products.ts
@@ -0,0 +1,21 @@
+{/* this is code used for an example of products the products will acctualy be a part of the backend
+    const products = [
+    {
+        id: 1,
+        name: "Umbrella",
+        category: "rainy"
+    },
+    {
+        id: 2,
+        name: "Raincoat",
+        category: "rainy"
+    },
+    {
+        id: 3,
+        name: "Water Bottle",
+        category: "sunny"
+    }
+]
+
+
+export default products;*/}
